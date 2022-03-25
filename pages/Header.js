98c3da00ea16427d9bd300da3af868c1fa0dc34e
Chevy <75184977+chavionjackson@users.noreply.github.com>
2022-03-25 12:05:30 -0700

    added content to header

--- pages/Header.js
+++ pages/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="pb-56 bg-black">
       <div className="headerContainer">
-        <header>
+        <header className="border-b-2">
           <div className="flex items-center justify-between h-10">
             <div className="flex items-center">
               <a href="#" className="m-6 text-sm text-white uppercase">
@@ -27,6 +27,25 @@ const Header = () => {
             </nav>
           </div>
         </header>
+        <div className="flex items-center justify-center text-center text-white test">
+          <span>
+            <h1 className="mt-32 text-6xl font-bold uppercase">
+              Premium Seating
+            </h1>
+            <p className="text-4xl uppercase text-[#fbed2a] mb-4">
+              The best way to enjoy all the excitment
+            </p>
+            <p className="mb-8 text-xl font-light leading-6">
+              Whether you’re entertaining business clients, closing a major
+              deal, or socializing with family and friends, a luxury suite
+              provides all the convenience, comfort and service you desire. As
+              the home of the Utah Jazz and the country’s most popular touring
+              musical acts and family shows, Vivint Arena is the place to meet,
+              greet, see and be seen in Utah.
+            </p>
+            <a className= "button">Contact Us</a>
+          </span>
+        </div>
       </div>
     </div>
   );
