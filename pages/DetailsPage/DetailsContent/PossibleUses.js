23e13161a2672d7a6d9a3f458cede2a3b06ae766
Chevy <75184977+chavionjackson@users.noreply.github.com>
2022-03-28 16:24:08 -0700

    created possible uses section

--- pages/DetailsPage/DetailsContent/PossibleUses.js
+++ pages/DetailsPage/DetailsContent/PossibleUses.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { AiFillPlusCircle } from "react-icons/ai";
+import Data from './UsesData'
 
 const PossibleUses = () => {
   return (
-    <div>PossibleUses</div>
-  )
+    <div className="pt-16 pb-16">
+      <span className="flex items-center mb-8">
+        <AiFillPlusCircle className="text-[#fbed2a] mr-2 text-3xl" />
+        <h2 className="text-3xl font-bold uppercase">Possible Uses</h2>
+      </span>
+      <div className="grid usesGrid gap-x-20">
+        {Data.map(({ header, content }) => (
+          <div key={"usesData"} className="flex flex-col w-40">
+            <h3 className="pb-2 text-xl font-bold usesSub">{header}</h3>
+            <ul>
+              {content.map((info) => (
+                <li key={"uses"} className="mt-4 mb-4 ml-auto mr-auto text-sm">
+                  {info}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PossibleUses
--- pages/DetailsPage/DetailsContent/UsesData.js
+++ pages/DetailsPage/DetailsContent/UsesData.js
@@ -48,3 +48,5 @@ const Data = [
     ],
   },
 ];
+
+export default Data;
